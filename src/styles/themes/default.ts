70dfdc9847f20b8c45667815544865ac665932d2
Davi Hasson Castro <davihasson@gmail.com>
2023-10-22 19:57:35 -0300

    chore: init project

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'blue': '#3294F8',
+  'base-title': '#EFEDF4',
+  'base-subtitle': '#C4D4E3',
+  'base-text': '#AFC2D4',
+
+  'base-span': '#7B96B2',
+  'base-label': '#3A536B',
+  'base-input': '#040F1A',
+  'base-border': '#1C2F41',
+  
+  'base-post': '#112131',
+  'base-profile': '#0B1B2B',
+  'base-background': '#071422'
+
+} as const
