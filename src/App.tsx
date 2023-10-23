d58dccbde278860eaba5a9b5a5a25baca0e7f2c5
Davi Hasson Castro <davihasson@gmail.com>
2023-10-22 21:03:51 -0300

    chore: routing the pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 import { DefaultLayout } from './layouts/DefaultLayout';
 
+import { BrowserRouter } from "react-router-dom";
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <DefaultLayout />
+      <BrowserRouter>
+        <DefaultLayout />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
