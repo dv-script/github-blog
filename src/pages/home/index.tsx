93743c497542990262f15b74514f1cae4e3a159c
Davi Hasson Castro <davihasson@gmail.com>
2023-10-23 20:24:21 -0300

    feat: adding ProfileCard to Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,44 @@
+import { useState, useEffect } from "react";
+import { instance } from "../../lib/axios";
 import * as S from "./styles";
+
 import { ProfileCard } from "./components/ProfileCard";
 
+interface IProfile {
+  login: string
+  avatar_url: string
+  html_url: string
+  name: string
+  bio: string
+  company: string
+  followers: number
+}
+
 export function Home() {
+
+  const [profile, setProfile] = useState({} as IProfile);
+
+  async function getProfile() {
+    const { data } = await instance.get(`/users/dv-script`);
+    setProfile(data);
+    return profile;
+  }
+  useEffect(() => {
+    getProfile();
+  }
+    , []);
+
   return (
     <S.Container>
-      <ProfileCard />
+      <ProfileCard
+        username={profile.login}
+        avatarUrl={profile.avatar_url}
+        name={profile.name}
+        bio={profile.bio}
+        company={profile.company}
+        followers={profile.followers}
+        htmlUrl={profile.html_url}
+      />
     </S.Container>
   );
 }
