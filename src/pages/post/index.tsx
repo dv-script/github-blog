a72f6e65ab8f1d86606b399aa3847d13b5f23759
Davi Hasson Castro <davihasson@gmail.com>
2023-10-25 15:15:23 -0300

    feat: new Post page

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,6 +1,10 @@
+import { PostHeader } from './components/PostHeader';
+import * as S from './styles';
+
 export function Post() {
     return (
-        <>
-        </>
+        <S.PostContainer>
+            <PostHeader />
+        </S.PostContainer>
     );
 }
