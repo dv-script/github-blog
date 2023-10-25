5f19f8f4437de8267ab936d3c275a97873de08b0
Davi Hasson Castro <davihasson@gmail.com>
2023-10-25 15:14:25 -0300

    style: delete unuseless width from ProfileCardContainer

--- src/pages/home/components/ProfileCard/styles.ts
+++ src/pages/home/components/ProfileCard/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 export const ProfileCardContainer = styled.div`
     position: relative;
-    width: 100%;
     display: flex;
     gap: 2rem;
     justify-content: center;
