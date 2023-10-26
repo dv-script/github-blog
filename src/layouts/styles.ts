b9b6223bc49c0d4a2b34707fadb9b66b97991d6d
Davi Hasson Castro <davihasson@gmail.com>
2023-10-25 23:55:25 -0300

    style: fixing the height style of Layout

--- src/layouts/styles.ts
+++ src/layouts/styles.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100%;
     height: 100%;
+    min-height: 100vh;
     background-color: ${({ theme }) => theme["base-input"]};
 `;
